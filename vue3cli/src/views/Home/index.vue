9b0c0675081e25557b96d4e80b2f7a0717a8df45
JoshuaZheng <3107641910@qq.com>
2023-04-05 20:50:55 +0800

    "4/5/23/1"

---
+++ vue3cli/src/views/Home/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="view"></div>
+</template>
+
+<script>
+export default {
+    name:'Home',
+}
+</script>
+
+<style>
+.view{
+    background: yellow;
+    height: 100%;
+    width: 100%;
+}
+</style>
